ef3bb440e1b11d87e82877a4ca39b635e0db638c
Marcio Coelho <marciocoelho@gmail.com>
2020-05-25 23:15:48 -0300

    JS updated

--- js/main.js
+++ js/main.js
@@ -1,127 +1,164 @@
-function corSite() {
-    if (status) {
-        document.querySelector("html").style.backgroundColor = "#fff"
-        document.querySelector("body").style.backgroundColor = "#222"
-        document.querySelector("body").style.color = "#fff"
-        document.querySelector("header").style.backgroundColor = "#000"
-        document.querySelector("header").style.borderBottomColor = "#fff"
-        document.querySelector("header > div > img").src = "img/logo-fatec-branco.png"
-        document.querySelector("header ul li.menu > ul.submenu").style.backgroundColor = "#000"
-        document.querySelectorAll("section#grade tr > th").forEach(function (j) {
-            j.style.borderColor = "#fff"
-        })
-        document.querySelector("section#video > video").style.borderColor = "#fff"
-        document.querySelectorAll("section#docentes aside > p > img").forEach(function (l) {
-            l.style.borderColor = "#fff"
-        })
-        document.querySelector("footer").style.backgroundColor = "#000"
-        document.querySelector("footer").style.color = "#fff"
-        document.querySelector("footer > form > button").style.backgroundColor = "#000"
-        document.querySelector("footer > form > button").style.color = "#fff"
-        document.querySelector("footer > form > button").style.borderColor = "#fff"
-    }
-    else {
-        document.querySelector("html").style.backgroundColor = "#385460"
-        document.querySelector("body").style.backgroundColor = "#ddd"
-        document.querySelector("body").style.color = "#385460"
-        document.querySelector("header").style.backgroundColor = "#222"
-        document.querySelector("header").style.borderBottomColor = "#af151e"
-        document.querySelector("header > div > img").src = "img/logo-fatec.png"
-        document.querySelector("header ul li.menu > ul.submenu").style.backgroundColor = "#222"
-        document.querySelectorAll("section#grade tr > th").forEach(function (k) {
-            k.style.borderColor = "#af151e"
-        })
-        document.querySelector("section#video > video").style.borderColor = "#af151e"
-        document.querySelectorAll("section#docentes aside > p > img").forEach(function (m) {
-            m.style.borderColor = "#af151e"
-        })
-        document.querySelector("footer").style.backgroundColor = "#222"
-        document.querySelector("footer").style.color = "#ddd"
-        document.querySelector("footer > form > button").style.backgroundColor = "#222"
-        document.querySelector("footer > form > button").style.color = "#ddd"
-        document.querySelector("footer > form > button").style.borderColor = "#ddd"
-    }
-    status = (status === 0) ? !0 : 0
-}
-
 function cursoFunc() {
-    curso.style.display = "block"
-    grade.style.display = "none"
-    video.style.display = "none"
-    docentes.style.display = "none"
-    obrigado.style.display = "none"
+    transitionSectionOut(curso)
+    transitionSectionIn(curso)
+    transitionSectionOut(grade)
+    transitionSectionOut(video)
+    transitionSectionOut(docentes)
+    transitionSectionOut(obrigado)
     document.querySelector("section#video video").pause()
 }
 
 function gradeFunc() {
-    curso.style.display = "none"
-    grade.style.display = "block"
-    video.style.display = "none"
-    docentes.style.display = "none"
-    obrigado.style.display = "none"
+    transitionSectionOut(grade)
+    transitionSectionOut(curso)
+    transitionSectionIn(grade)
+    transitionSectionOut(video)
+    transitionSectionOut(docentes)
+    transitionSectionOut(obrigado)
     document.querySelector("section#video video").pause()
 }
 
 function videoFunc() {
-    curso.style.display = "none"
-    grade.style.display = "none"
-    video.style.display = "block"
-    docentes.style.display = "none"
-    obrigado.style.display = "none"
+    transitionSectionOut(video)
+    transitionSectionOut(curso)
+    transitionSectionOut(grade)
+    transitionSectionIn(video)
+    transitionSectionOut(docentes)
+    transitionSectionOut(obrigado)
 }
 
 function docentesFunc() {
-    curso.style.display = "none"
-    grade.style.display = "none"
-    video.style.display = "none"
-    docentes.style.display = "block"
-    obrigado.style.display = "none"
-    document.querySelector("section#docentes > main").style.display = "grid"
-    document.querySelector("section#docentes > aside + p").style.display = "none"
-    document.querySelector("section#video video").pause()
-    if (reset !== 99) {
-        docentes.getElementsByTagName("p")[reset + 22].style.display = "none"
+    transitionSectionOut(docentes)
+    transitionSectionOut(curso)
+    transitionSectionOut(grade)
+    transitionSectionOut(video)
+    transitionSectionIn(docentes)
+    transitionSectionOut(obrigado)
+    if (resetDocentes < document.querySelectorAll("section#docentes > nav > p").length) {
+        transitionSectionOut(document.querySelectorAll("section#docentes aside > p")[resetDocentes])
+        transitionSectionOut(document.querySelector("section#docentes > aside + p"))
     }
+    transitionSectionIn(document.querySelector("section#docentes > nav"), true)
+    document.querySelector("section#video video").pause()
 }
 
 function showPic(n) {
-    if (reset !== 99) {
-        docentes.getElementsByTagName("p")[reset + 22].style.display = "none"
+    transitionSectionOut(document.querySelector("section#docentes nav"))
+    transitionSectionIn(document.querySelector("section#docentes > aside + p"))
+    transitionSectionIn(document.querySelectorAll("section#docentes aside > p")[n])
+    resetDocentes = n
+}
+
+function contraste() {
+    document.querySelector("html").style.backgroundColor = statusContraste ? "#fff" : "#385460"
+    document.querySelector("body").style.backgroundColor = statusContraste ? "#222" : "#ddd"
+    document.querySelector("body").style.color = statusContraste ? "#fff" : "#385460"
+    document.querySelector("header").style.backgroundColor = statusContraste ? "#000" : "#222"
+    document.querySelector("header").style.borderBottomColor = statusContraste ? "#fff" : "#af151e"
+    document.querySelector("header > div > img").src = statusContraste ? "img/logo-fatec-branco.png" : "img/logo-fatec.png"
+    document.querySelector("header ul li.menu > ul.submenu").style.backgroundColor = statusContraste ? "#000" : "#222"
+    document.querySelectorAll("section#grade > nav > ul > li > a").forEach(function (j) {
+        j.style.color = statusContraste ? "#fff" : "#ddd"
+        j.style.backgroundColor = statusContraste ? "#000" : "#385460"
+    })
+    document.querySelector("section#video > video").style.borderColor = statusContraste ? "#fff" : "#af151e"
+    document.querySelectorAll("section#docentes > nav > p").forEach(function(k) {
+        k.style.outlineColor = statusContraste ? "#fff" : "#af151e"
+    })
+    document.querySelectorAll("section#docentes aside > p > img").forEach(function (l) {
+        l.style.borderColor = statusContraste ? "#fff" : "#af151e"
+    })
+    document.querySelectorAll("section#config li > a").forEach(function(m) {
+        m.style.backgroundColor = statusContraste ? "#000" : "#222"
+    })
+    document.querySelector("footer").style.backgroundColor = statusContraste ? "#000" : "#222"
+    document.querySelector("footer").style.color = statusContraste ? "#fff" : "#385460"
+    document.querySelector("footer").style.borderTopColor = statusContraste ? "#fff" : "#af151e"
+    document.querySelector("footer > form > button").style.backgroundColor = statusContraste ? "#000" : "#222"
+    statusContraste = ! statusContraste
+}
+
+function fonte(tam) {
+    if (tam === 0) {
+        document.querySelector("body").style.fontSize = `${inicialFonte = 1}em`
+        limiteFonte = 0
     }
-    document.querySelector("section#docentes > main").style.display = "none"
-    docentes.getElementsByTagName("p")[n + 22].style.display = "block"
-    document.querySelector("section#docentes > aside + p").style.display = "block"
-    reset = n
+    else if (tam === 1) {
+        if (limiteFonte < 7) {
+            document.querySelector("body").style.fontSize = `${inicialFonte += 0.1}em`
+            limiteFonte++
+        }
+        else {
+            document.querySelector("section#config li > a#fonteMais").style.border = "1px solid #af151e"
+            setTimeout(function() {
+                document.querySelector("section#config li > a#fonteMais").style.border = "0"
+            }, 200)
+        }
+    }
+    else if (tam === -1) {
+        if (limiteFonte > -7) {
+            document.querySelector("body").style.fontSize = `${inicialFonte -= 0.1}em`
+            limiteFonte--
+        }
+        else {
+            document.querySelector("section#config li > a#fonteMenos").style.border = "1px solid #af151e"
+            setTimeout(function() {
+                document.querySelector("section#config li > a#fonteMenos").style.border = "0"
+            }, 200)
+        }
+    }
 }
 
 function obrigadoFunc() {
-    curso.style.display = "none"
-    grade.style.display = "none"
-    video.style.display = "none"
-    docentes.style.display = "none"
-    obrigado.style.display = "block"
+    transitionSectionOut(obrigado)
+    transitionSectionOut(curso)
+    transitionSectionOut(grade)
+    transitionSectionOut(video)
+    transitionSectionOut(docentes)
+    transitionSectionIn(obrigado)
     document.querySelector("section#video video").pause()
 }
 
-let status = true
-let reset = 99
-let contraste = document.getElementById("contraste")
-let curso = document.getElementById("curso")
-let grade = document.getElementById("grade")
-let video = document.getElementById("video")
-let docentes = document.getElementById("docentes")
-let obrigado = document.getElementById("obrigado")
-contraste.addEventListener("click", function() { corSite() })
-document.querySelectorAll("header > nav li a")[0].addEventListener("click", function() { cursoFunc() })
-document.querySelectorAll("header > nav li a")[1].addEventListener("click", function() { gradeFunc() })
-document.querySelectorAll("header > nav li a")[2].addEventListener("click", function() { videoFunc() })
-document.querySelectorAll("header > nav li a")[3].addEventListener("click", function() { docentesFunc() })
-document.querySelectorAll("header > nav li a")[5].addEventListener("click", function() { cursoFunc() })
-document.querySelectorAll("header > nav li a")[6].addEventListener("click", function() { gradeFunc() })
-document.querySelectorAll("header > nav li a")[7].addEventListener("click", function() { videoFunc() })
-document.querySelectorAll("header > nav li a")[8].addEventListener("click", function() { docentesFunc() })
-for (let i = 0; i <= 22; i++) {
+function transitionSectionIn(targetIn, grid=false) {
+    targetIn.style.display = grid ? "grid" : "block"
+    window.setTimeout(function() {
+        targetIn.style.opacity = "1"
+        targetIn.style.transform = "scale(1)"
+    }, 200)
+}
+
+function transitionSectionOut(targetOut) {
+    targetOut.style.display = "none"
+    targetOut.style.opacity = "0"
+    targetOut.style.transform = "scale(0)"
+}
+
+let curso = document.querySelector("section#curso")
+let grade = document.querySelector("section#grade")
+let video = document.querySelector("section#video")
+let docentes = document.querySelector("section#docentes")
+let resetDocentes = document.querySelectorAll("section#docentes > nav > p").length + 1
+let obrigado = document.querySelector("section#obrigado")
+let statusContraste = true
+let inicialFonte = 1
+let limiteFonte = 0
+document.querySelectorAll("a.curso").forEach(function (e) {
+    e.addEventListener("click", function() { cursoFunc() })
+})
+document.querySelectorAll("a.grade").forEach(function (f) {
+    f.addEventListener("click", function() { gradeFunc() })
+})
+document.querySelectorAll("a.video").forEach(function (g) {
+    g.addEventListener("click", function() { videoFunc() })
+})
+document.querySelectorAll(".docentes").forEach(function (h) {
+    h.addEventListener("click", function() { docentesFunc() })
+})
+for (let i = 0; i <= document.querySelectorAll("section#docentes > nav > p").length; i++) {
     docentes.getElementsByTagName("p")[i].addEventListener("click", function() { showPic(i) })
 }
-document.querySelector("section#docentes > aside + p").addEventListener("click", function () { docentesFunc() })
+document.querySelector("section#config li > a#contraste").addEventListener("click", function() { contraste() })
+document.querySelector("section#config li > a#fonteMais").addEventListener("click", function() { fonte(1) })
+document.querySelector("section#config li > a#fonteReset").addEventListener("click", function() { fonte(0) })
+document.querySelector("section#config li > a#fonteMenos").addEventListener("click", function() { fonte(-1) })
 document.querySelector("footer > form > button").addEventListener("click", function() { obrigadoFunc() })
